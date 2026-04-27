2e60653d81208a2b727f8fb1e7682bbffaf16f6d
aqppann <aqppann@gmail.com>
2026-04-27 20:35:46 +0300

    feat: initial Spring Boot project setup

---
+++ src/main/java/com/aqppann/sqlanalyzer/SqlAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.aqppann.sqlanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aqppann/sqlanalyzer/SqlAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aqppann.sqlanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
